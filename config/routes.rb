70a0d6fe400a3774aa5630c27f58978b99961694
Ben <lyenliang@gmail.com>
2016-05-15 13:53:26 +0000

    Add more functions

--- config/routes.rb
+++ config/routes.rb
@@ -1,13 +1,18 @@
 Rails.application.routes.draw do
 
+  devise_for :users
+  root 'products#index'
+
   namespace :admin do
     resources :products
   end
+  
+  resources :products
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
   # You can have the root of your site routed with "root"
-  # root 'welcome#index'
+  
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
