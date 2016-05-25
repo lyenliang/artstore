4cc02b0de631b7803a5dda5587ddf3ccea103200
Ben <lyenliang@gmail.com>
2016-05-25 07:58:43 +0000

    You can't buy items with 0 number

--- app/controllers/cart_items_controller.rb
+++ app/controllers/cart_items_controller.rb
@@ -12,11 +12,18 @@ class CartItemsController < ApplicationController
         redirect_to :back
     end
     
+    # update quantity under "products/id" path
     def update
         @cart = current_cart
         @item = @cart.cart_items.find_by(product_id: params[:id])
         
-        @item.update(item_params)
+        # 若 庫存 >= 買家指定的數量
+        if @item.product.quantity >= item_params[:quantity].to_i
+            @item.update(item_params)
+            flash[:notice] = "成功變更數量"
+        else
+            flash[:warning] = "庫存不足"
+        end
         
         redirect_to carts_path
     end
