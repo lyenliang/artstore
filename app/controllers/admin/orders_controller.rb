60190a10c3da6d3fd9996aef19510fa83f401f89
Ben <lyenliang@gmail.com>
2016-05-28 13:06:03 +0000

    Act according to AASM state

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,4 +9,33 @@ class Admin::OrdersController < ApplicationController
         @orders = Order.all.order("id DESC")
     end
     
+    def show
+        @order = Order.find(params[:id])
+        @order_info = @order.info
+        @order_items = @order.items
+    end
+    
+    def cancel
+        @order = Order.find(params[:id])
+        @order.cancel_order!
+        redirect_to :back
+    end
+    
+    def ship
+        @order = Order.find(params[:id])
+        @order.ship!
+        redirect_to :back
+    end
+    
+    def shipped
+        @order = Order.find(params[:id])
+        @order.deliver!
+        redirect_to :back
+    end
+    
+    def return
+        @order = Order.find(params[:id])
+        @order.return_goods!
+        redirect_to :back
+    end
 end
