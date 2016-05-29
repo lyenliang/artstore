0ac15b17c58399c0d58195ad5a0933c4c35a7ac2
Ben <lyenliang@gmail.com>
2016-05-29 02:28:36 +0000

    Add products to cart

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -10,4 +10,30 @@ class ApplicationController < ActionController::Base
     end
   end
 
+  helper_method :current_cart
+  
+  def current_cart
+    @current_cart ||= find_cart
+  end
+  
+  private 
+  
+  def find_cart
+    cart = Cart.find_by(id: session[:cart_id])
+    
+    unless cart.present?
+      cart = Cart.create
+      # Can I put "session[:cart_id] = cart.id" here?
+    end
+    
+    logger.debug "cart.id: #{cart.id}"
+    logger.debug "session[:cart_id]: #{session[:cart_id]}"
+    if (cart.id != session[:cart_id]) 
+      logger.debug "difference found"
+    end
+    session[:cart_id] = cart.id
+    
+    return cart
+  end
+
 end
