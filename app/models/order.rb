29b76c7af2c56f8e93546d432e7fb7765815e0a7
Ben <lyenliang@gmail.com>
2016-05-30 08:34:58 +0000

    Add aasm

--- app/models/order.rb
+++ app/models/order.rb
@@ -29,4 +29,43 @@ class Order < ActiveRecord::Base
         self.total = cart.total_price
         self.save
     end
+    
+    def set_payment_with!(payment_method)
+        self.update_columns(payment_method: payment_method)
+    end
+    
+    def pay!
+        self.update_columns(is_paid: true)
+    end
+    
+    include AASM
+    
+    aasm do
+        state :order_placed, initial: true
+        state :paid
+        state :shipping
+        state :shipped
+        state :order_cancelled
+        state :goods_returned
+        
+        event :make_payment, after_commit: :pay! do
+            transitions from: :order_placed, to: :paid
+        end
+        
+        event :ship do
+            transitions from: :paid, to: :shipping
+        end
+        
+        event :deliver do
+            transitions from: :shipping, to: :shipped
+        end
+        
+        event :return_good do
+            transitions from: :shipped, to: goods_returned
+        end
+        
+        event :cancel_order do
+            transitions from: [:order_placed, :paid], to: :order_cancelled
+        end
+    end
 end
