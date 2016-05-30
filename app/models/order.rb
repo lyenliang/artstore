303a42db2d7b73d472acd0540f7022a79a0f493c
Ben <lyenliang@gmail.com>
2016-05-30 04:01:45 +0000

    Create orders

--- app/models/order.rb
+++ app/models/order.rb
@@ -5,4 +5,21 @@ class Order < ActiveRecord::Base
     has_one :info, class_name: "OrderInfo", dependent: :destroy
     
     accepts_nested_attributes_for :info
+    
+    def build_item_cache_from_cart(cart)
+        # cart_item is a product
+        cart.items.each do |cart_item|
+            # item is a OrderItem
+            item = items.build
+            item.product_name = cart_item.title
+            item.price = cart_item.price
+            item.quantity = 1
+            item.save
+        end 
+    end
+    
+    def calculate_total!(cart)
+        self.total = cart.total_price
+        self.save
+    end
 end
